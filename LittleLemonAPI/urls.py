d930cdb8593eff573e0ea6836e90a3b85822018f
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-06-25 02:05:02 +0200

    by using generic view classes, added two views, one serializer and to urls

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ratings', views.RatingsView,as_view()),
+    #path('ratings', views.RatingsView.as_view()),
+    path('menu-items', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view())
 ]
